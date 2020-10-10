e06c15e331bff4d7627ddad556b276d74813429d
Andrés Arias <arias9306@gmail.com>
2020-10-10 08:59:05 -0500

    ci(auth): a

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
+    'subject-empty': [2, 'never'],
     'header-min-length':[2, 'always', 10],
     'body-min-length': [2, 'always', 10],
     'scope-enum': [2, 'always', [
